1aa4a3c07ae8d0aeca8b48b8ec93febffde776c2
19muskan19 <muskanpatel505@gmail.com>
2024-07-28 15:11:18 +0530

    new repo

---
+++ p2.java
@@ -0,0 +1,13 @@
+//Write a Java program to print the sum of two numbers & divide two numbers and print them on the screen.
+
+public class p2{
+    public static void main(String[] args) {
+        int a=40,b=20;
+        int sum=a+b;
+        int div=a/b;
+        System.out.println("Sum of two numbers is "+sum);
+        System.out.println("Division of two numbers is "+div);
+    }
+        
+
+}
